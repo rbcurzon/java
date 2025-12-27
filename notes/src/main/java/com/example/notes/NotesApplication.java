3f9b28329370920301fcdfcff1edb5c4b6cd843f
Ronald B. Curzon <curzonronald@gmail.com>
2025-12-27 11:19:21 +0800

    feat: add H2 database configuration

--- notes/src/main/java/com/example/notes/NotesApplication.java
+++ notes/src/main/java/com/example/notes/NotesApplication.java
@@ -1,12 +1,7 @@
 package com.example.notes;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.notes.entity.Note;
-import com.example.notes.repository.NoteRepository;
 
 @SpringBootApplication
 public class NotesApplication {
@@ -15,14 +10,14 @@ public class NotesApplication {
 		SpringApplication.run(NotesApplication.class, args);
 	}
 
-    @Bean
-    CommandLineRunner loadData(NoteRepository repository) {
-		return (args) -> {
-			repository.save(new Note("Oscar Wilde","*Be yourself*; everyone else is already taken."));
-			repository.save(new Note("Frank Zappa", "So many books, *so little time.*"));
-			repository.save(new Note("Marcus Tulius Cicero", "A room without books is like a body without a soul."));
-		};
-		
-	}
+//    @Bean
+//    CommandLineRunner loadData(NoteRepository repository) {
+//		return (args) -> {
+//			repository.save(new Note("Oscar Wilde","*Be yourself*; everyone else is already taken."));
+//			repository.save(new Note("Frank Zappa", "So many books, *so little time.*"));
+//			repository.save(new Note("Marcus Tulius Cicero", "A room without books is like a body without a soul."));
+//		};
+//		
+//	}
 
 }
